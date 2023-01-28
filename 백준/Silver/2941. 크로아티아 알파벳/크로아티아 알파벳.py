ea7d37d3d0c8c06d8ce22d51f0d9a8278ce66465
seong-un <122589861+seong-un@users.noreply.github.com>
2023-01-29 00:43:22 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -0,0 +1,12 @@
+alphabet=input()
+
+alphabet=alphabet.replace("dz=", "/")
+alphabet=alphabet.replace("z=", "/")
+alphabet=alphabet.replace("c=", "/")
+alphabet=alphabet.replace("c-", "/")
+alphabet=alphabet.replace("d-", "/")
+alphabet=alphabet.replace("lj", "/")
+alphabet=alphabet.replace("nj", "/")
+alphabet=alphabet.replace("s=", "/")
+
+print(len(alphabet))
