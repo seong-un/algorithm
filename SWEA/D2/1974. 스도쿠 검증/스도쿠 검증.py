59d7fa161821812a1fbde2a72abdf3eceb2b0235
seong-un <122589861+seong-un@users.noreply.github.com>
2023-01-30 11:22:42 +0900

    [D2] Title: 스도쿠 검증, Time: 162 ms, Memory: 58,508 KB -BaekjoonHub

---
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -0,0 +1,21 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    sudoku=[]
+    result=1
+    for i in range(9):
+        sudoku.append(list(map(int, input().split())))
+    for j in range(9):
+        for i in range(1,10,1):
+            if i not in sudoku[j]:
+                result=0
+    for j in range(9):
+        for i in range(1,10,1):
+            if i not in [k[j] for k in sudoku]:
+                result=0
+    for i in range(3):
+        for j in range(3):
+            for k in range(1,10,1):
+                if k not in [sudoku[3*i][3*j], sudoku[3*i][3*j+1], sudoku[3*i][3*j+2], sudoku[3*i+1][3*j], sudoku[3*i+1][3*j+1], sudoku[3*i+1][3*j+2], sudoku[3*i+2][3*j], sudoku[3*i+2][3*j+1], sudoku[3*i+2][3*j+2]]:
+                    result=0
+    print(f"#{test_case} {result}")
