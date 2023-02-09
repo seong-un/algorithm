38c244d016c69c31242ff4ddf9115829a0381de7
seong-un <122589861+seong-un@users.noreply.github.com>
2023-02-09 11:28:01 +0900

    [Bronze I] Title: 색종이, Time: 400 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,19 +1,24 @@
-N=int(input())
-colored_paper=[0]*N
+N = int(input())
+colored_paper = [0] * N
 for i in range(N):
-    colored_paper[i]=list(map(int, input().split()))
-A=[set() for i in range(N)]
-for idx, i in enumerate(colored_paper):
-    for j in range(i[0], i[0]+i[2]):
-        for k in range(i[1], i[1]+i[3]):
-            A[idx].add((j,k))
-area=[0]*N
-
-for idx, i in enumerate(A):
-    sum_area=set()
-    for j in A[idx+1:]:
-        sum_area=sum_area|j
-    area[idx]=len(i)-len(i&sum_area)
-
-for i in area:
+    colored_paper[i] = list(map(int, input().split()))
+horizon, vertex=0,0
+for i in range(N):
+    if horizon<colored_paper[i][1]+colored_paper[i][3]:
+        horizon=colored_paper[i][1]+colored_paper[i][3]
+for i in range(N):
+    if vertex<colored_paper[i][0]+colored_paper[i][2]:
+        vertex=colored_paper[i][0]+colored_paper[i][2]
+plane = [[0] * (horizon) for _ in range(vertex)]
+for i in range(1, N + 1):
+    for j in range(colored_paper[i - 1][0], colored_paper[i - 1][0] + colored_paper[i - 1][2]):
+        plane[j][colored_paper[i - 1][1]:colored_paper[i - 1][1] + colored_paper[i - 1][3]] = [i] * (
+            colored_paper[i - 1][3])
+numbers = [0] * N
+for i in plane:
+    for j in i:
+        for k in range(N):
+            if j == k + 1:
+                numbers[k] += 1
+for i in numbers:
     print(i)
