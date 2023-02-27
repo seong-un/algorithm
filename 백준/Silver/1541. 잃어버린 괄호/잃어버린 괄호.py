e17210fd50376a3b59c5ff1b37532a8f5e75d6e8
seong-un <122589861+seong-un@users.noreply.github.com>
2023-02-27 14:36:57 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -2,12 +2,11 @@ mod=input()
 if '-' in mod:
     mod2=mod[:mod.index('-')]
     mod=mod[mod.index('-')+1:]
-    sum_mod2=0
+    sm=0
     for i in list(mod2.split('+')):
-        sum_mod2+=sum(map(int, i.split('-')))
-    sum_mod=0
+        sm+=sum(map(int, i.split('-')))
     for i in list(mod.split('+')):
-        sum_mod+=sum(map(int, i.split('-')))
-    print(sum_mod2-sum_mod)
+        sm-=sum(map(int, i.split('-')))
+    print(sm)
 else:
     print(sum(map(int, mod.split('+'))))
