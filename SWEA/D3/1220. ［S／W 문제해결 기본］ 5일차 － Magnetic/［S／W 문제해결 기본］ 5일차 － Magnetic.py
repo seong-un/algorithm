17184c19edc059977e2339150c7c58cfe5f1bc66
seong-un <122589861+seong-un@users.noreply.github.com>
2023-03-01 01:19:30 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 235 ms, Memory: 63,312 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -2,15 +2,9 @@ for test_case in range(1, 11):
     N=int(input())
     table=[0]*N
     for i in range(N):
-        table[i]=list(map(int, input().split()))
+        table[i]=list(input().split())
     table=list(zip(*table))
-    deadlock=0
+    cnt=0
     for i in table:
-        magnetic=False
-        for j in i:
-            if j==1 and not magnetic:
-                magnetic=True
-            if magnetic and j==2:
-                deadlock+=1
-                magnetic=False
-    print(f'#{test_case} {deadlock}')
+        cnt+=''.join(i).replace('0', '').count('12')
+    print(f'#{test_case} {cnt}')
