35df9d61b8952f0b1145a9d43d6aa6e53ad1b7ed
seong-un <122589861+seong-un@users.noreply.github.com>
2023-03-13 03:32:41 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,36 @@
+notation = list(input())
+result = ''
+stack = []
+for idx, i in enumerate(notation):
+  if 65 <= ord(i) <= 90:
+    result+=i
+  elif i=='(':
+    stack.append(i)
+  elif i in ['+', '-']:
+    if not stack:
+      stack.append(i)
+    else:
+      if stack[-1]=='(':
+        stack.append(i)
+      else:
+        while stack and stack[-1]!='(':
+          result+=stack.pop()
+        stack.append(i)
+  elif i in ['*', '/']:
+    if not stack:
+      stack.append(i)
+    else:
+      if stack[-1]=='(':
+        stack.append(i)
+      elif stack[-1] in ['+', '-']:
+        stack.append(i)
+      else:
+        result+=stack.pop()
+        stack.append(i)
+  else:
+    while stack[-1]!='(':
+      result+=stack.pop()
+    stack.pop()
+while stack:
+  result+=stack.pop()
+print(result)
