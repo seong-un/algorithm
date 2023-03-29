101bb7dffa72df98b72656bc7201aada16b9c9b5
seong-un <122589861+seong-un@users.noreply.github.com>
2023-03-29 15:46:24 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 356 ms, Memory: 58376 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,7 +1,10 @@
-N=int(input())
-coordinate=[]
-for _ in range(N):
-    coordinate.append(list(map(int, input().split())))
-coordinate=sorted(coordinate, key=lambda x:(x[0], x[1]))
-for i in coordinate:
-    print(*i)
+import sys
+one = []
+n = int(input())
+for i in range(n):
+    one.append(list(map(int,sys.stdin.readline().split())))
+
+one = sorted(one, key = lambda x: (x[0], x[1]))
+
+for i in one:
+    print(f'{i[0]} {i[1]}')
