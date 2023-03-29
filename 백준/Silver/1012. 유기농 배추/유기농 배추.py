7adb288d62a4e00e325d457e6217e580c107e256
seong-un <122589861+seong-un@users.noreply.github.com>
2023-03-29 17:40:28 +0900

    [Silver II] Title: 유기농 배추, Time: 312 ms, Memory: 31620 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,38 +1,35 @@
 import sys
-sys.setrecursionlimit(1000000)
-T=int(input())
+sys.setrecursionlimit(10000)
 
-for _ in range(T):
-  N, M, K=map(int, input().split())
-  graph=[[0]*M for _ in range(N)]
-  for _ in range(K):
-    start, end = map(int,input().split())
-    graph[start][end] = 1
-  
-  def dfs(n,m):
-    graph[n][m]=0
-    if n<N-1:
-      if graph[n+1][m]==1:
-        graph[n+1][m]=0
-        dfs(n+1,m)
-    if n>0:
-      if graph[n-1][m]==1:
-        graph[n-1][m]=0
-        dfs(n-1,m)
-    if m>0:
-      if graph[n][m-1]==1:
-        graph[n][m-1]=0
-        dfs(n,m-1)
-    if m<M-1:
-      if graph[n][m+1]==1:
-        graph[n][m+1]=0
-        dfs(n,m+1)
-  
-  ji=0
-  for i in range(N):
-    for j in range(M):
-      if graph[i][j]==1:
-        ji+=1
-        dfs(i,j)
-  
-  print(ji)
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+def dfs(x, y):
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < M and 0 <= ny < N:
+            if area[nx][ny] == 1:
+                area[nx][ny] = 0
+                dfs(nx, ny)
+
+T = int(input())
+for tc in range(T):
+    M, N, K = map(int,input().split())
+    area = [[0 for _ in range(N)] for _ in range(M)]
+    cnt = 0
+
+    for k in range(K):
+        x, y = map(int, input().split())
+        area[x][y] = 1
+    # pprint(area)
+
+    for x in range(M):
+        for y in range(N):
+            if area[x][y] == 1:
+                dfs(x,y)
+                cnt += 1  
+
+    print(cnt)
