578d2fee423d7d483b116cc75621d87b0574bf6e
seong-un <122589861+seong-un@users.noreply.github.com>
2023-03-31 01:32:57 +0900

    [D3] Title: N-Queen, Time: 1,546 ms, Memory: 65,420 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,16 +1,14 @@
+from collections import deque
+
 def check(i, j):
-    checkit=True
-    for k in range(8):
-        q=1
-        while True:
-            nx=i+dx[k]*q
-            ny=j+dy[k]*q
-            if nx<0 or ny<0 or nx>N-1 or ny>N-1:
-                break
-            if (nx, ny) in queen:
-                checkit=False
-            q+=1
-    return checkit
+    for k in queen:
+        if k[1]==j:
+            return False
+        if k[0]-k[1]==i-j:
+            return False
+        if k[0]+k[1]==i+j:
+            return False
+    return True
 
 
 def Queen(i, N):
@@ -22,14 +20,12 @@ def Queen(i, N):
         if check(i, j):
             queen.append((i,j))
             Queen(i+1,N)
-            del queen[queen.index((i,j))]
+            queen.pop()
 
 T=int(input())
 for test_case in range(1, T+1):
-    N=int(input())
-    cnt=0
-    queen=[]
-    dx=[-1, 1, 0, 0, -1, -1, 1, 1]
-    dy=[0, 0, -1, 1, -1, 1, -1, 1]
-    Queen(0, N)
-    print(f'#{test_case} {cnt}')
+        N=int(input())
+        cnt=0
+        queen=deque()
+        Queen(0, N)
+        print(f'#{test_case} {cnt}')
