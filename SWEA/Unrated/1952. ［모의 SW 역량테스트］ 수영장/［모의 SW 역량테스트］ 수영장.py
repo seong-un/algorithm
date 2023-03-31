a08476002c046d87f741e09f2c38494953f4a881
seong-un <122589861+seong-un@users.noreply.github.com>
2023-03-31 13:48:13 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 159 ms, Memory: 58,776 KB -BaekjoonHub

---
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -0,0 +1,17 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    day, month, mon3, year = map(int, input().split())
+    plan = list(map(int, input().split()))
+    dp = [0] * 12
+
+    dp[0] = min(plan[0] * day, month, mon3)
+    dp[1]=min((plan[0]+plan[1])*day, min([plan[0], plan[1]])*day+month, month*2, mon3)
+    dp[2]=min((plan[0]+plan[1]+plan[2])*day, min([plan[0], plan[1], plan[2]])*day+month*2, min([plan[0]+plan[1], plan[0]+plan[2], plan[1]+plan[2]])*day+month, month*3, mon3)
+
+    for i in range(3, 12):
+        dp[i] = min(dp[i - 1] + plan[i] * day, dp[i - 1] + month, dp[i - 3] + mon3)
+
+    dp[10] = min(dp[10], dp[9] + mon3)
+    dp[11] = min(dp[11], dp[10] + mon3, dp[9] + mon3)
+
+    print(f'#{test_case} {min(dp[-1], year)}')
