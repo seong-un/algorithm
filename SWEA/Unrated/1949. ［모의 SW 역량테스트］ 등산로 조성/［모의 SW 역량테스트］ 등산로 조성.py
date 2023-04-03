305c3b4a9d49602d12ad7a2d9b2ec46ea5356384
seong-un <122589861+seong-un@users.noreply.github.com>
2023-04-03 16:25:52 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 262 ms, Memory: 64,656 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
@@ -1,7 +1,5 @@
-def dfs(i, j, K, cnt, mt):
+def dfs(i, j, K, cnt):
     global depth
-    if K<0:
-        return
     for k in range(4):
         nx=i+dx[k]
         ny=j+dy[k]
@@ -9,13 +7,13 @@ def dfs(i, j, K, cnt, mt):
             continue
         if mt[i][j]>mt[nx][ny]:
             visited.append((nx, ny))
-            dfs(nx, ny, K, cnt+1, mt)
+            dfs(nx, ny, K, cnt+1)
             visited.pop()
         elif 0<=mt[nx][ny]-mt[i][j]<K:
             visited.append((nx, ny))
             origin=mt[nx][ny]
             mt[nx][ny]=mt[i][j]-1
-            dfs(nx, ny, 0, cnt+1, mt)
+            dfs(nx, ny, 0, cnt+1)
             mt[nx][ny]=origin
             visited.pop()
     depth=max(depth, cnt)
@@ -37,5 +35,5 @@ for test_case in range(1, T+1):
         for j in range(N):
             if mt[i][j]==summit:
                 visited=[(i, j)]
-                dfs(i, j, K, 1, mt)
+                dfs(i, j, K, 1)
     print(f'#{test_case} {depth}')
