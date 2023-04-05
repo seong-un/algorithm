c6e1842a3e1ee2874f00c270998a2e0ef7f447e1
seong-un <122589861+seong-un@users.noreply.github.com>
2023-04-05 13:16:26 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 330 ms, Memory: 64,404 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -0,0 +1,27 @@
+import heapq
+
+T=int(input())
+dx=[-1, 1, 0, 0]
+dy=[0, 0, -1, 1]
+INF=int(1e9)
+for test_case in range(1, T+1):
+    N=int(input())
+    Map=[list(map(int, list(input()))) for i in range(N)]
+    least=[[INF]*N for i in range(N)]
+    a,b=0,0
+    least[a][b]=0
+    heap=[]
+    heapq.heappush(heap, [0, a, b])
+    while heap:
+        cost, a, b=heapq.heappop(heap)
+        if a==N-1 and b==N-1:
+            print(f'#{test_case} {cost}')
+        for k in range(4):
+            nx=a+dx[k]
+            ny=b+dy[k]
+            if nx<0 or ny<0 or nx>N-1 or ny>N-1:
+                continue
+            dist=least[a][b]+Map[nx][ny]
+            if dist<least[nx][ny]:
+                least[nx][ny]=dist
+                heapq.heappush(heap, [dist, nx, ny])
