ec028e5b4a3c4064731881433f1c9c3ce4135dfc
seong-un <122589861+seong-un@users.noreply.github.com>
2023-04-20 13:31:54 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,4 @@
+const fs = require("fs")
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let [a, b] = fs.readFileSync(filePath).toString().split(" ").map(string => {return Number(string)});
+console.log(a-b)
