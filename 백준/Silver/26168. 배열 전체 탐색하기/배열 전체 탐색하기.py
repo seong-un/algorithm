5ad4ddce405f1470084e83aa12c7b9d18d312790
seong-un <122589861+seong-un@users.noreply.github.com>
2023-05-05 17:52:35 +0900

    [Silver IV] Title: 배열 전체 탐색하기, Time: 168 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.py
+++ 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.py
@@ -5,35 +5,79 @@ for _ in range(m):
     Q, *rng=map(int, input().split())
     for_con=False
     if Q==1:
-        for idx, i in enumerate(A):
-            if i>=rng[0]:
-                print(n-idx)
-                for_con=True
-                break
-        if not for_con:
+        start=0
+        end=n
+        try:
+            while start<=end:
+                mid=(start+end)//2
+                if A[mid]>=rng[0]:
+                    if A[mid-1]<rng[0]:
+                        print(n-mid)
+                        for_con=True
+                        break
+                    else:
+                        end=mid-1
+                else:
+                    start=mid+1
+            if not for_con:
+                print(n)
+        except:
             print(0)
     elif Q==2:
-        for idx, i in enumerate(A):
-            if i>rng[0]:
-                print(n-idx)
-                for_con=True
-                break
-        if not for_con:
+        start=0
+        end=n
+        try:
+            while start<=end:
+                mid=(start+end)//2
+                if A[mid]>rng[0]:
+                    if A[mid-1]<=rng[0]:
+                        print(n-mid)
+                        for_con=True
+                        break
+                    else:
+                        end=mid-1
+                else:
+                    start=mid+1
+            if not for_con:
+                print(n)
+        except:
             print(0)
     else:
-        for idx, i in enumerate(A):
-            if i>=rng[0]:
-                a=idx
-                for_con=True
-                break
-        if not for_con:
+        start=0
+        end=n
+        a=n
+        try:
+            while start<=end:
+                mid=(start+end)//2
+                if A[mid]>=rng[0]:
+                    if A[mid-1]<rng[0]:
+                        a=mid
+                        for_con=True
+                        break
+                    else:
+                        end=mid-1
+                else:
+                    start=mid+1
+            if not for_con:
+                a=0
+        except:
             print(0)
             continue
-        for_con=False
-        for jdx, j in enumerate(A[a:]):
-            if j>rng[1]:
-                print(jdx)
-                for_con=True
-                break
-        if not for_con:
-            print(n-idx)
+        start=0
+        end=n
+        b=n
+        try:
+            while start<=end:
+                mid=(start+end)//2
+                if A[mid]<=rng[1]:
+                    if A[mid+1]>rng[1]:
+                        b=mid
+                        break
+                    else:
+                        start=mid+1
+                else:
+                    end=mid-1
+        except:
+            print(n-a)
+            continue
+        print(b-a+1)
