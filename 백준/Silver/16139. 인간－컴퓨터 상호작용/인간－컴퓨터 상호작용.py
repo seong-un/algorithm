23c5edd01135cbd7735a6d5ab32ddfdfd5f63aa6
seong-un <122589861+seong-un@users.noreply.github.com>
2023-07-21 01:43:49 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 236 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,15 +1,24 @@
 S = input()
 Q = int(input())
-alphabet = [[0] * (len(S) + 1) for i in range(26)]
+alphabet = [[0] * len(S) for i in range(26)]
 for i in range(26):
     for j in range(len(S)):
         if chr(i + 97) == S[j]:
-            alphabet[i][j+1] = alphabet[i][j] + 1
+            if j == 0:
+                alphabet[i][j] = 1
+            else:
+                alphabet[i][j] = alphabet[i][j-1] + 1
         else:
-            alphabet[i][j+1] = alphabet[i][j]
+            if j == 0:
+                alphabet[i][j] = 0
+            else:
+                alphabet[i][j] = alphabet[i][j-1]
 
 for q in range(Q):
     a, l, r = input().split()
     l = int(l)
     r = int(r)
-    print(alphabet[ord(a) - 97][r + 1] - alphabet[ord(a) - 97][l])
+    if l != 0:
+        print(alphabet[ord(a) - 97][r] - alphabet[ord(a) - 97][l - 1])
+    else:
+        print(alphabet[ord(a) - 97][r])
